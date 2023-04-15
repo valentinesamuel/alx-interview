c385150a776d8f12104a8a744783ee60369f57de
valentinesamuel <valentinesamuel2580@gmail.com>
2023-04-15 11:58:40 +0100

    Added desc

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,45 +1,49 @@
 #!/usr/bin/python3
-""" This scripts read each line from the stdin and calculates the meterics """
+"""
+This script that reads stdin line by line and computes metrics
+"""
 
-import sys
+def parseLogs():
+    """
+    Reads logs from standard input and generates reports
+    Raises:
+        KeyboardInterrupt (Exception): handles this exception and raises it
+    """
+    stdin = __import__('sys').stdin
+    lineNumber = 0
+    fileSize = 0
+    statusCodes = {}
+    codes = ('200', '301', '400', '401', '403', '404', '405', '500')
+    try:
+        for line in stdin:
+            lineNumber += 1
+            line = line.split()
+            try:
+                fileSize += int(line[-1])
+                if line[-2] in codes:
+                    try:
+                        statusCodes[line[-2]] += 1
+                    except KeyError:
+                        statusCodes[line[-2]] = 1
+            except (IndexError, ValueError):
+                pass
+            if lineNumber == 10:
+                report(fileSize, statusCodes)
+                lineNumber = 0
+        report(fileSize, statusCodes)
+    except KeyboardInterrupt as e:
+        report(fileSize, statusCodes)
+        raise
 
-def print__http_status_logs(dic, size):
-    """ Prints information """
-    print("File size: {:d}".format(size))
-    for i in sorted(dic.keys()):
-        if dic[i] != 0:
-            print("{}: {:d}".format(i, dic[i]))
 
-http_status_array = {
-    "200": 0, 
-    "301": 0, 
-    "400": 0, 
-    "401": 0, 
-    "403": 0,
-    "404": 0, 
-    "405": 0, 
-    "500": 0
-}
+def report(fileSize, statusCodes):
+    """
+    This function Prints generated report to standard output
+    """
+    print("File size: {}".format(fileSize))
+    for key, value in sorted(statusCodes.items()):
+        print("{}: {}".format(key, value))
 
-count = 0
-size = 0
 
-try:
-    for ln in sys.stdin:
-        if count != 0 and count % 10 == 0:
-            print__http_status_logs(http_status_array, size)
-        status_list = ln.split()
-        count += 1
-        try:
-            size += int(status_list[-1])
-        except:
-            pass
-        try:
-            if status_list[-2] in http_status_array:
-                http_status_array[status_list[-2]] += 1
-        except:
-            pass
-    print__http_status_logs(http_status_array, size)
-except KeyboardInterrupt:
-    print__http_status_logs(http_status_array, size)
-    raise
+if __name__ == '__main__':
+    parseLogs()
